08d8d9e946c28056881cc63481c3c9b403d35b47
bvstivn <bbmmllvv@gmail.com>
2024-03-24 10:39:39 -0300

    first commit

---
+++ src/App.vue
@@ -0,0 +1,56 @@
+<template>
+  <Header :carrito="carrito" @incrementar-cantidad="incrementarCantidad" @decrementar-cantidad="decrementarCantidad"/>
+
+  <main class="container-xl mt-5">
+    <h2 class="text-center">Nuestra Colección</h2>
+
+    <div class="row mt-5">
+      <Guitarra
+        v-for="guitarra in guitarras"
+        :guitarra="guitarra"
+        @agregar-carrito="agregarCarrito"
+      />
+    </div>
+  </main>
+
+  <Footer />
+</template>
+
+<script setup>
+//Vue
+import { ref, onMounted } from "vue";
+import { db } from "../src/data/guitarras";
+//Components
+import Guitarra from "../src/components/Guitarra.vue";
+import Header from "./components/Header.vue";
+import Footer from "./components/Footer.vue";
+
+const guitarras = ref([]);
+const carrito = ref([]);
+
+onMounted(() => {
+  guitarras.value = db;
+});
+
+const agregarCarrito = (guitarra) => {
+  const existeCarrito = carrito.value.findIndex(
+    (producto) => producto.id === guitarra.id
+  );
+  if (existeCarrito >= 0) {
+    carrito.value[existeCarrito].cantidad++;
+  } else {
+    guitarra.cantidad = 1;
+    carrito.value.push(guitarra);
+  }
+};
+
+const decrementarCantidad = () => {
+
+}
+
+const incrementarCantidad = () => {
+  
+}
+</script>
+
+<style scoped></style>
